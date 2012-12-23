6412e4a6bca8e7ca033b785e9814a62e20506706
jinesh <jineshpaloor@gmail.com>
2012-12-23 22:29:54 +0500

    liked pages complete

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ SECRET_KEY = 'development key'
 DEBUG = True
 FACEBOOK_APP_ID = '453146034732597'
 FACEBOOK_APP_SECRET = '83459d1711daff7eaeb6412f635e5abc'
+query_url = 'https://graph.facebook.com/fql?'
+
 
 app = Flask(__name__)
 app.debug = DEBUG
@@ -111,49 +113,106 @@ def home():
     return render_template('home.html')
 
 
+
 @app.route('/get_photos')
 def get_photos():
-    return facebook.authorize(callback=url_for('facebook_authorized',
+    return facebook.authorize(callback=url_for('facebook_get_photos',
         next=request.args.get('next') or request.referrer or None,
         _external=True))
 
 
+@app.route('/get_pages')
+def get_pages():
+    return facebook.authorize(callback=url_for('facebook_get_pages',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+def get_access_token(resp):
+    session['oauth_token'] = (resp['access_token'], '')
+    return resp['access_token']
+
+def get_most_liked_photos_query():
+    return "SELECT pid, caption, aid, owner, link, src_big, src_small, created, modified, like_info FROM photo \
+            WHERE created > 1325356200 and aid IN \
+            (SELECT aid FROM album WHERE owner IN \
+            (SELECT uid2 FROM friend WHERE uid1=me()))ORDER BY like_info.like_count DESC LIMIT 30"
+
+
+def get_most_liked_pages_query():
+    return "SELECT name, fan_count,about,categories, description, general_info, \
+            page_id, page_url, pic_big, pic_cover, pic_large, type, website \
+            FROM page WHERE page_id IN (SELECT page_id FROM page_fan \
+            WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me())) ORDER BY fan_count DESC LIMIT 30"
+
+def encode_for_url(data):
+    return urllib.urlencode(data)
+
+def get_data_from_url(url):
+    return urllib2.urlopen(url)
+
 @app.route('/fb/photos')
 @facebook.authorized_handler
-def facebook_authorized(resp):
+def facebook_get_photos(resp):
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
             request.args['error_description']
         )
-    session['oauth_token'] = (resp['access_token'], '')
-    access_token = resp['access_token']
 
-#    current_dir = os.path.realpath(__file__)
+    access_token = get_access_token(resp)
     current_dir = os.getcwd()
-    filepath = str(current_dir)+'/cache/'+str(access_token)+'.txt'
+    filepath = str(current_dir)+'/cache/'+str(access_token)+'_photos.txt'
+
     if os.path.exists(filepath):
         with open(filepath, 'r') as fp:
             fb_liked_photos = fp.read()
             fb_liked_photos = json.loads(fb_liked_photos)
     else:
         #query to get liked photos of user and his friends
-        liked_photos_query = "SELECT pid, caption, aid, owner, link, src_big, src_small, created, modified, like_info FROM photo WHERE created > 1325356200 and aid IN \
-            (SELECT aid FROM album WHERE owner IN \
-            (SELECT uid2 FROM friend WHERE uid1=me()))ORDER BY like_info.like_count DESC LIMIT 30"
+        data = {'q':get_most_liked_photos_query(), 'access_token':access_token}
+        encoded_data = encode_for_url(data)
 
-        data = {'q':liked_photos_query, 'access_token':access_token}
-        encode_data = urllib.urlencode(data)
-        query_url = 'https://graph.facebook.com/fql?%s' % encode_data
-        fetch_data = urllib2.urlopen(query_url)
-        fb_liked_photos = json.load(fetch_data)
-        fetch_data.close()
+        url = query_url+encoded_data
+        liked_photos_list = get_data_from_url(url)
+        fb_liked_photos = json.load(liked_photos_list)
+        liked_photos_list.close()
 
-        with open('cache/'+access_token+'.txt', 'w') as f:
+        with open('cache/'+access_token+'_photos.txt', 'w') as f:
             f.write(json.dumps(fb_liked_photos))
 
-    return render_template('details.html', fb_liked_photos=fb_liked_photos['data'])
+    return render_template('photos.html', fb_liked_photos=fb_liked_photos['data'])
 
+@app.route('/fb/pages')
+@facebook.authorized_handler
+def facebook_get_pages(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+
+    access_token = get_access_token(resp)
+    current_dir = os.getcwd()
+    filepath = str(current_dir)+'/cache/'+str(access_token)+'_pages.txt'
+
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as fp:
+            fb_liked_pages = fp.read()
+            fb_liked_pages = json.loads(fb_liked_pages)
+    else:
+        #query to get liked photos of user and his friends
+        data = {'q':get_most_liked_pages_query(), 'access_token':access_token}
+        encoded_data = encode_for_url(data)
+
+        url = query_url+encoded_data
+        liked_pages_list = get_data_from_url(url)
+        fb_liked_pages = json.load(liked_pages_list)
+        liked_pages_list.close()
+
+        with open('cache/'+access_token+'_pages.txt', 'w') as f:
+            f.write(json.dumps(fb_liked_pages))
+
+    return render_template('pages.html', fb_liked_pages=fb_liked_pages['data'])
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
